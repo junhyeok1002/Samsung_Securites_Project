fc041fd4457185157ef6ce93e130a1eb47d75731
Jun Hyeok Choi <113586985+junhyeok1002@users.noreply.github.com>
2023-06-06 20:14:07 +0900

    Add files via upload

--- Samsung_Securities_Project_Main.py
+++ Samsung_Securities_Project_Main.py
@@ -475,7 +475,7 @@ if __name__ == "__main__":
         
         # 포트폴리오 추천탭 예외 처리
         except Exception as e : 
-            st.warning(e)
+            st.warning(str(e))
     
     # Streamlit 흐름 제어를 위한 부분
     elif case == 'blank':
